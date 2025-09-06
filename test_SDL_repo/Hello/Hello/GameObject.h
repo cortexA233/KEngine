b25e9aa701b84678b49b16f40327d4d3ab0f4976
cortexA233 <cortexA233@outlook.com>
2025-09-06 14:40:54 -0400

    update

---
+++ test_SDL_repo/Hello/Hello/GameObject.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <SDL3_image/SDL_image.h>
+
+
+class GameObject
+{
+public:
+	GameObject() = default;
+	virtual ~GameObject() = default;
+	
+};
+
---
+++ test_SDL_repo/Hello/Hello/PlayerCharacter.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "GameObject.h"
+
+class PlayerCharacter : public GameObject
+{
+public:
+};
+
